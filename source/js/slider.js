6fbce8dfe3f258ddce156a2230b708e197e3bc99
rook-a <zhernovkov_konst@mail.ru>
2021-08-09 21:25:31 +0300

    исправил ошибки и доделал слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,18 +1,22 @@
-const clipbox = document.querySelector(".sample__slider");
-const range = document.querySelector(".slider__range");
-const first   = document.querySelector(".sample__img--before");
+const clipbox = document.querySelector('.sample__slider');
+const range   = document.querySelector('.slider__range');
+const dragger = document.querySelector('.slider__dragger');
+const first   = document.querySelector('.sample__img--before');
 // onClick
 const imgBefore = document.querySelector('.sample__img--before');
-const imgAfter = document.querySelector('.sample__img--after');
+const imgAfter  = document.querySelector('.sample__img--after');
 const imgActive = document.querySelector('.sample__img--active');
-const btnPrev = document.getElementById('btnPrev');
-const btnNext = document.getElementById('btnNext');
+const btnPrev   = document.getElementById('btnPrev');
+const btnNext   = document.getElementById('btnNext');
 
 btnPrev.addEventListener('click', function() {
   if (imgAfter.classList.contains('sample__img--active')) {
     imgAfter.classList.remove('sample__img--active');
     imgBefore.classList.add('sample__img--active');
   }
+
+  btnNext.classList.remove('sample__btn--next');
+  btnPrev.classList.add('sample__btn--prev');
 });
 
 btnNext.addEventListener('click', function() {
@@ -20,6 +24,9 @@ btnNext.addEventListener('click', function() {
     imgBefore.classList.remove('sample__img--active');
     imgAfter.classList.add('sample__img--active');
   }
+
+  btnPrev.classList.remove('sample__btn--prev');
+  btnNext.classList.add('sample__btn--next');
 });
 
 //
@@ -34,32 +41,33 @@ const clipboxDimensions = {
 
 const handleStartDrag = () => {
   drag = true;
-  // dragger.classList.add("slider__gragger--active");
-  range.style.pointerEvents = "none";
+  range.style.pointerEvents = 'none';
+  dragger.style.pointerEvents = 'none';
 };
 
 const handleStopDrag = () => {
   drag = false;
-  range.style.pointerEvents = "auto";
-  // dragger.classList.remove("slider__gragger--active");
-  // clipbox.style.cursor = "auto";
+  range.style.pointerEvents = 'auto';
+  dragger.style.pointerEvents = 'auto';
+  // clipbox.style.cursor = 'auto';
 };
 
 const handleImgReveal = e => {
   e.preventDefault();
   e.offsetX = e.offsetX || e.targetTouches[0].pageX - clipboxDimensions.left;
   if(drag && e.offsetX < clipboxDimensions.width && e.offsetX > 0) {
-    // clipbox.style.cursor = "grab";
-    range.style.left = e.offsetX - draggerWidth / 2 + "px";
-    first.style.width = e.offsetX + "px";
+    // clipbox.style.cursor = 'grab';
+    range.style.left = e.offsetX - draggerWidth / 2 + 'px';
+    first.style.width = e.offsetX + 'px';
+    dragger.style.left = e.offsetX + (draggerWidth / 100 - 17) + 'px';
   }
 };
 
-range.addEventListener("mousedown", handleStartDrag);
-range.addEventListener("touchstart", handleStartDrag);
+range.addEventListener('mousedown', handleStartDrag);
+range.addEventListener('touchstart', handleStartDrag);
 
-clipbox.addEventListener("mouseup", handleStopDrag);
-clipbox.addEventListener("touchend", handleStopDrag);
+clipbox.addEventListener('mouseup', handleStopDrag);
+clipbox.addEventListener('touchend', handleStopDrag);
 
-clipbox.addEventListener("mousemove", handleImgReveal);
-clipbox.addEventListener("touchmove", handleImgReveal);
+clipbox.addEventListener('mousemove', handleImgReveal);
+clipbox.addEventListener('touchmove', handleImgReveal);
