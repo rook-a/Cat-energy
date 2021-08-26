f0a6490fd804b247e8cad215be191e62a178da89
rook-a <zhernovkov_konst@mail.ru>
2021-08-26 16:26:00 +0300

    исправил ошибки stylelint и обновил slider.js

--- source/js/slider.js
+++ source/js/slider.js
@@ -6,17 +6,33 @@ const btnPrev   = document.getElementById('btnPrev');
 const btnNext   = document.getElementById('btnNext');
 
 btnPrev.addEventListener('click', function() {
+  if (imgAfter.classList.contains('sample__img--active')) {
+    imgAfter.classList.remove('sample__img--active');
+    imgBefore.classList.add('sample__img--active');
+  }
+
+  btnNext.classList.remove('sample__btn--next');
+  btnPrev.classList.add('sample__btn--prev');
+
   dragger.style.left = 0 + '%';
-
-  imgBefore.style.width = 0 + '%';
-  imgAfter.style.width = 100 + '%';
-});
-
-btnNext.addEventListener('click', function() {
-  dragger.style.left = 100 + '%';
 
   imgBefore.style.width = 100 + '%';
   imgAfter.style.width = 0 + '%';
+});
+
+btnNext.addEventListener('click', function() {
+  if (imgBefore.classList.contains('sample__img--active')) {
+    imgBefore.classList.remove('sample__img--active');
+    imgAfter.classList.add('sample__img--active');
+  }
+
+  btnPrev.classList.remove('sample__btn--prev');
+  btnNext.classList.add('sample__btn--next');
+
+  dragger.style.left = 100 + '%';
+
+  imgBefore.style.width = 0 + '%';
+  imgAfter.style.width = 100 + '%';
 });
 
 function size() {
